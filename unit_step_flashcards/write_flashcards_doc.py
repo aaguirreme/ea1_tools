2fce3e89e864a0856c4ee14b5fb777c8509901f8
Andrés Mauricio Aguirre-Mesa <aaguirr2@gmail.com>
2020-02-21 18:34:08 -0600

    Last minute commit

---
+++ unit_step_flashcards/write_flashcards_doc.py
@@ -0,0 +1,46 @@
+
+# User parameters
+# ===============
+
+# Number of plots.
+N = 11
+
+filename = 'flashcards_unit_step.tex'
+
+preamble = r'''\documentclass[grid, poker_portrait]{flashcards}
+
+\usepackage{graphicx}
+\usepackage{mathrsfs}
+\usepackage{breqn}
+
+\cardfrontstyle[\Huge]{plain}
+\cardbackstyle[\Huge]{plain}
+'''
+
+# Main script
+
+# Write the preamble and open the document environment.
+with open(filename, 'w') as fid:
+    fid.write(preamble + '\n')
+    fid.write(r'\begin{document}' + '\n\n')
+# Close file.
+
+# Write flashcards.
+for k in range(1,N+1):
+
+    with open(filename, 'a') as fid:
+        fid.write(\
+fr'''    %{k}
+    \begin{{flashcard}}{{ \begin{{dmath*}} \input{{ example{k}.txt }} \end{{dmath*}} }}
+        \includegraphics[width=8cm]{{example{k}unitstep.pdf}}
+    \end{{flashcard}}
+''')
+        fid.write('\n')
+    # Close file. 
+
+# End for. Write flashcards.
+
+# Write the statement to close the document environment.
+with open(filename, 'a') as fid:
+    fid.write(r'\end{document}')
+# Close file.
